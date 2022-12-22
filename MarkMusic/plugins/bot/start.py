4b0da26a67c4806b931084b130345465071c9ab8
Levi <115207729+mcstan85@users.noreply.github.com>
2022-12-23 00:35:32 +0530

    Update start.py

--- MarkMusic/plugins/bot/start.py
+++ MarkMusic/plugins/bot/start.py
@@ -39,7 +39,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAEBSCFjot5yk2PKWoL45pu4hg0YtGf26wACHAgAAljCGVXAyAZtHyfjQywE")
+            await message.reply_sticker("CAACAgEAAxkBAAENzLtjpKn-4anGneskJtzjJ7w_KHumQgACSgMAAuwSIEX6uJby0aceVSwE")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -188,7 +188,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAEBSCFjot5yk2PKWoL45pu4hg0YtGf26wACHAgAAljCGVXAyAZtHyfjQywE")
+                await message.reply_sticker("CAACAgEAAxkBAAENzLtjpKn-4anGneskJtzjJ7w_KHumQgACSgMAAuwSIEX6uJby0aceVSwE")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
