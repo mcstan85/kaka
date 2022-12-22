480a95dcb0b48ac363bae7e409aa4b51bc506d68
Levi <115207729+mcstan85@users.noreply.github.com>
2022-12-23 00:52:31 +0530

    Update start.py

--- MarkMusic/utils/inline/start.py
+++ MarkMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="» Aᴅᴅ ᴍᴇ ʙᴀʙʏ «",
+                text="» Aᴅᴅ ᴍᴇ ᴅᴏꜱᴛ «",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
@@ -38,7 +38,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="» Aᴅᴅ ᴍᴇ ʙᴀʙʏ «",
+                text="» Aᴅᴅ ᴍᴇ ᴅᴏꜱᴛ «",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             ),
         ], 
